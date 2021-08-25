5c471de0da6b549288497c30491530fe9c22c1cf
Xiaoxia Che <chexiaoxia@gmail.com>
2021-08-25 00:04:10 -0400

    Add Hanoi function

--- recursive.cpp
+++ recursive.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+long step;
+
 long fibonacci(int n)
 {
     if(n == 0 || n == 1)
@@ -20,6 +22,21 @@ long factorial(int n)
     return n * factorial(n - 1);
 }
 
+/**
+ * Move n disks from left rod to middle rod using the right rod.
+ */
+void hanoi(int n, char left, char middle, char right)
+{
+    if (n == 0)
+    {
+        return;
+    }	    
+    hanoi(n-1, left, right, middle);
+    cout << step << ". " << left << " -> " << middle << endl;
+    step++;
+    hanoi(n-1, right, middle, left);
+}
+
 int main()
 {
     cout << "Factorial of 4 is: " << factorial(4) << endl;
@@ -27,10 +44,18 @@ int main()
     cout << "Factorial of 20 is: " << factorial(20) << endl;
     
     cout << "------------------------------------" << endl;
-    for(int i = 0; i < 50; i++)
+    for(int i = 0; i < 20; i++)
     {
         cout << fibonacci(i) << ", ";
     }
     cout << endl;
+
+    cout << "------------------------------------" << endl;
+    cout << "Hanoi game of 3 plates:" << endl;
+    step = 1;
+    hanoi(3, 'A', 'B', 'C');
+    cout << "Hanoi game of 15 plates:" << endl;
+    step = 1;
+    hanoi(15, 'A', 'B', 'C');
     return 0;
 }
